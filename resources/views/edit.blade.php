d6828ec95126cf20a74f85856522505a112abc06
gentanaka <gen.tanaka2001@gmail.com>
2022-01-02 01:51:04 +0900

    section6
    メモ機能の細かい部分を実装

--- resources/views/edit.blade.php
+++ resources/views/edit.blade.php
@@ -1,21 +1,28 @@
 @extends('layouts.app')
 
+@section('javascript')
+    <script src="/js/confirm.js"></script>
+@endsection
+
 @section('content')
 <div class="card">
     <div class="card-header">
         メモ編集
-        <form class="card-body" action="{{ route('destroy') }}" method="POST">
+        <form class="card-body" id="delete-form" action="{{ route('destroy') }}" method="POST">
             @csrf
             <input type="hidden" name="memo_id" value="{{ $edit_memo[0]['id'] }}">
-            <button type="submit">削除</button>
+            <button type="submit" onclick="deleteHandle(event);">削除</button>
         </form>
     </div>
     <form class="card-body" action="{{ route('update') }}" method="POST">
         @csrf
         <input type="hidden" name="memo_id" value="{{ $edit_memo[0]['id'] }}">
-        <div class="form-group">
+        <div class="form-group mb-3">
             <textarea class="form-control" name="content" rows="3" placeholder="ここにメモを入力">{{ $edit_memo[0]['content'] }}</textarea>
         </div>
+        @error('content')
+            <div class="alert alert-danger">メモ内容を入力してください</div>
+        @enderror
         @foreach ($tags as $tag)
             <div class="form-check form-check-inline mb-3">
                 <input type="checkbox" class="form-check-input" name="tags[]" id="{{ $tag['id'] }}" value="{{ $tag['id'] }}" {{ in_array($tag['id'], $include_tags) ? 'checked' : '' }}>
